019a99da49becae1eb641b5ad7c71ae9688cabd7
saudi <saud.vectorcoder@gmail.com>
2020-06-16 12:03:59 +0500

    Fabric to Firebase - v1.5

--- app/src/main/java/com/sunztech/ibnemaja/MyApplication.java
+++ app/src/main/java/com/sunztech/ibnemaja/MyApplication.java
@@ -2,13 +2,9 @@ package com.sunztech.ibnemaja;
 
 import android.app.Activity;
 import android.app.Application;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
-import com.crashlytics.android.Crashlytics;
 import com.onesignal.OneSignal;
-
-import io.fabric.sdk.android.Fabric;
 
 public class MyApplication extends Application {
 
@@ -17,7 +13,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
 
         // OneSignal Initialization
         OneSignal.startInit(this)
